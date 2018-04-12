c2113f2fe027b5820ca614d7cc8abdad59ca8c4a
Anthony Savant Moore <savant@frwdinc.com>
2018-04-12 12:13:32 -0400

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,7 +8,7 @@ const Landing = () =>  {
     return (
         <Grid id="Landing" className="container-fluid">
           <Jumbotron id="landing-jumbotron">
-            <h1>FRWD(pronounced FORWARD)</h1>
+            <h1>Finances Require Winners Discipline</h1>
             <h6>If you save 14% of your income you will be a millionaire by 65
             </h6>
             <Button className="nav-btn">
